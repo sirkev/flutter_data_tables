f9fcf0c0729a013dfd2559889df21a1225c6025e
kev <kevinoduor8877@gmail.com>
2023-04-21 12:48:50 +0300

    appointments table

---
+++ lib/data.dart
@@ -0,0 +1,36 @@
+List<String> days = [
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+  'Sunday',
+];
+List<String> staff = [
+  'Staff 1',
+  'Staff 2',
+  'Staff 3',
+  'Staff 4',
+  'Staff 5',
+  'Staff 6',
+  'Staff 7',
+];
+
+List<String> staffNames = ['Jordan Anatol', 'Wendy Smith', 'Kelly Jones'];
+
+List<DateTime> dates = [
+  DateTime(2023, 1, 1),
+  DateTime(2023, 1, 2),
+  DateTime(2023, 1, 3),
+  DateTime(2023, 1, 4),
+  DateTime(2023, 1, 5),
+  DateTime(2023, 1, 6),
+  DateTime(2023, 1, 7),
+];
+
+Map<String, dynamic> getEmployees() {
+  return {
+    // "employees": {"count": 3, "l"}
+  };
+}
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'data.dart';
+
 class HomePage extends StatefulWidget {
   final String title;
   const HomePage({Key? key, required this.title}) : super(key: key);
@@ -9,8 +11,90 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late ScrollController _verticalScrollController;
+  late ScrollController _horizontalScrollController;
+  @override
+  void initState() {
+    super.initState();
+    _verticalScrollController = ScrollController();
+    _horizontalScrollController = ScrollController();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.symmetric(vertical: 100, horizontal: 16),
+        height: 200,
+        child: Scrollbar(
+          controller: _verticalScrollController,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            controller: _verticalScrollController,
+            child: Scrollbar(
+              controller: _horizontalScrollController,
+              child: Scrollbar(
+                controller: _verticalScrollController,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: DataTable(
+                      border: TableBorder.all(
+                          color: Colors.grey[400] as Color,
+                          borderRadius: BorderRadius.circular(10)),
+                      columns: _getDayColumns(),
+                      rows: _getEmployeeRows()),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<DataCell> _getEmployeeNameCells() {
+    List<DataCell> cells = [];
+    for (String employeeName in staffNames) {
+      cells.add(DataCell(Text(employeeName)));
+    }
+    return cells;
+  }
+
+  List<DataRow> _getEmployeeRows() {
+    List<DataRow> rows = [];
+    for (String staff in staffNames) {
+      rows.add(DataRow(cells: [
+        DataCell(Text(staff)),
+        DataCell(Text('')),
+        DataCell(Text('')),
+        DataCell(Text('')),
+        DataCell(Text('')),
+        DataCell(Text('')),
+        DataCell(Text('')),
+        DataCell(Text('')),
+      ]));
+    }
+
+    return rows;
+  }
+
+  List<DataColumn> _getDayColumns() {
+    List<DataColumn> columns = [];
+    columns.add(
+      DataColumn(
+          label: Text(
+        'Change Staff',
+        style:
+            TextStyle(color: Colors.amberAccent, fontWeight: FontWeight.w700),
+      )),
+    );
+    for (String day in days) {
+      columns.add(
+        DataColumn(label: Text(day.toUpperCase())),
+      );
+    }
+    print('${columns.length}');
+
+    return columns;
   }
 }
