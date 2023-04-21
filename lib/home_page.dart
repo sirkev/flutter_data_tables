97ae25c1a7a47ee1eaa9213f6be1a18637c46a77
kev <kevinoduor8877@gmail.com>
2023-04-21 08:29:02 +0300

    home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,6 +11,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
